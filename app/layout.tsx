978945b5ebe340c35066b02e45dce8a9ce98c9ee
manthan-web <gamingmn1102@gmail.com>
2025-07-10 14:15:44 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
